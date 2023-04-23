a2af346d1f123e3de8a9da4c49717eaf76c450dc
Mr Thimote <mrthimote@gmail.com>
2023-04-23 13:27:41 +0200

    dev-graph: added output links to input

--- webdrom/editor/graph/graph.js
+++ webdrom/editor/graph/graph.js
@@ -1,8 +1,14 @@
 
-function append_drag_listener (element, callback) {
+function append_drag_listener (element, callback, end_callback = undefined) {
     let integralX = 0;
     let integralY = 0;
+    element.setAttribute("useDrag", "true");
+
     element.addEventListener("mousedown", (event) => {
+        let target = event.target;
+        while (target && (!target.hasAttribute("useDrag"))) target = target.parentNode;
+        if (target != element) return ;
+
         let sx = event.clientX; let sy = event.clientY;
         
         document.onmousemove = (event) => {
@@ -17,11 +23,15 @@ function append_drag_listener (element, callback) {
             
             sx = x; sy = y;
         }
+
+        document.onmouseup = (event) => {
+            document.onmousemove = undefined;
+    
+            if (end_callback)
+                end_callback(integralX, integralY);
+        };
     });
 }
-document.addEventListener("mouseup", (event) => {
-    document.onmousemove = undefined;
-});
 function neg_mod (h, mod) {
     if (h >= 0) return h % mod;
 
@@ -34,7 +44,7 @@ function cap (x, min_x, max_x) {
     return x;
 }
 
-const GRAPH_BACKGROUND_TILING = 25;
+const GRAPH_BACKGROUND_TILING = 45;
 
 /**
  * Molyb Graph-Based Editor
@@ -43,20 +53,33 @@ class MGraph extends Component {
     constructor (parent, engine) {
         super(parent);
 
-        this.nodes = [ new MNode(this, "Default MNode", 0, 0, [
-            new MNode_Ressource(this, "Input 1", "#3c3c3c"),
-            new MNode_Ressource(this, "Input 2", "#3c3c3c")
+        this.nodes = [ new MNode(this, "Default MNode", 200, 200, [
+            new MNode_Ressource(this, this, "Input 1", "float", "#7DAF9C"),
+            new MNode_Ressource(this, this, "Input 2", "int",   "#0A2472")
         ], [
-            new MNode_Ressource(this, "Output", "#3c3c3c", true)
+            new MNode_Ressource(this, this, "Output",  "float", "#7DAF9C", true)
         ]),
-        new MNode(this, "Default MNode", 400, 0, [
-            new MNode_Ressource(this, "Input 1", "#3c3c3c"),
-            new MNode_Ressource(this, "Input 2", "#3c3c3c")
+        new MNode(this, "Default MNode", 200, 600, [
+            new MNode_Ressource(this, this, "Input 1", "float", "#7DAF9C"),
+            new MNode_Ressource(this, this, "Input 2", "int",   "#0A2472")
         ], [
-            new MNode_Ressource(this, "Output", "#3c3c3c", true)
+            new MNode_Ressource(this, this, "Output",  "float", "#7DAF9C", true)
+        ]),
+        new MNode(this, "Default MNode", 600, 200, [
+            new MNode_Ressource(this, this, "Input 1", "float", "#7DAF9C"),
+            new MNode_Ressource(this, this, "Input 2", "float", "#7DAF9C")
+        ], [
+            new MNode_Ressource(this, this, "Output",  "int",   "#0A2472", true)
         ]) ];
 
+        this.current_ressource = undefined;
         this._first_render();
+    }
+    ressource_hover (res) {
+        this.current_ressource = res;
+    }
+    ressource_end_hover () {
+        this.current_ressource = undefined;
     }
     _first_render () {
         this.bg_element = createElement("div", {}, "w-full h-full absolute forward-grid-background", []);
@@ -85,15 +108,67 @@ class MGraph extends Component {
 }
 
 class MNode_Ressource extends Component {
-    constructor (parent, name, color, is_output = false) {
+    constructor (parent, graph, name, node_type, color, is_output = false) {
         super(parent);
 
-        this.left  = left;
+        this.graph     = graph;
+        this.is_output = is_output;
+
+        this.node_type = node_type;
+
         this.name  = name;
         this.color = color;
 
+        // Graph links
+        this.parent = undefined;
+        this.childs = new Set();
+
+        this.node = undefined;
+
         this._first_render();
     }
+    setNode (node) {
+        this.node = node;
+    }
+    removeChild (child) {
+        this.childs.delete(child);
+        this.setDebugColor();
+    }
+    appendChild (child) {
+        this.childs.add(child);
+        this.setDebugColor();
+    }
+    appendParent (parent) {
+        if (parent === undefined) {
+            if (this.parent) this.parent.removeChild(this);
+
+            this.parent = undefined;
+            this.setDebugColor();
+            return ;
+        }
+
+        if (this.is_output
+         || parent.node == this.node
+         || this.node_type != parent.node_type) return false;
+        if (this.parent) this.parent.removeChild(this);
+
+        this.parent = parent;
+        this.parent.appendChild(this);
+        this.setDebugColor();
+    }
+    setDebugColor () {
+        let is_debug = ((!this.is_output) && (this.parent !== undefined))
+                    || (this.is_output && this.childs.size != 0);
+        console.log(is_debug, this.parent, this.childs, this.is_output);
+        let color = is_debug ? "#6c6c6c" : this.color;
+
+        this.bubble.style.backgroundColor = color;
+    }
+    clear () {
+        for (let child of this.childs) child.appendParent(undefined);
+        if (this.parent) this.appendParent(undefined);
+    }
+
     _first_render () {
         this.bubble  = createElement("div", {}, `acenter-h ${this.is_output ? "right" : "left"}-[-20px] rounded-200 w-2 h-2`, []);
         this.bubble.style.backgroundColor = this.color;
@@ -101,9 +176,29 @@ class MNode_Ressource extends Component {
         this.element = createElement("div", {}, "relative", [
             createElement("div", {}, "select-none text-sm font-300", [
                 this.bubble,
+                createElement("div", {}, `absolute h-[100%] w-6 ${this.is_output ? "right-[-24px]" : "left-[-24px]"}`, []),
                 this.name
             ])
         ]);
+
+        if (!this.is_output) {
+            this.element.addEventListener("mouseover", (event) => this.graph.ressource_hover(this));
+            this.element.addEventListener("mouseout", (event) => this.graph.ressource_end_hover());
+        } else {
+            append_drag_listener(this.element, (dx, dy, ix, iy) => {  }, (ix, iy) => {
+                let input  = this;
+                let output = this.graph.current_ressource;
+                if (!output) return ;
+
+                output.appendParent(input);
+            })
+        }
+
+        this.element.addEventListener("contextmenu", (event) => {
+            event.preventDefault();
+
+            this.clear();
+        })
     }
     _render () { return this.element; }
 }
@@ -115,6 +210,8 @@ class MNode extends Component {
 
         this.inputs  = inputs;
         this.outputs = outputs;
+        for (let input  of this.inputs ) input .setNode(this);
+        for (let output of this.outputs) output.setNode(this);
 
         this.x    = x;
         this.y    = y;
